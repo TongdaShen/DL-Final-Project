c0ba561bf8740827de9ce6f192682c0947a69a51
TongdaShen <113846393+TongdaShen@users.noreply.github.com>
2023-05-09 08:36:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -35,9 +35,12 @@ def tensor_to_image(tensor):
      
 ##Download images and choose a style image and a content image
 
-content_path = tf.keras.utils.get_file('YellowLabradorLooking_new.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/YellowLabradorLooking_new.jpg')
-style1_path = tf.keras.utils.get_file('kandinsky5.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
-style2_path = 'Mondriaan.jpg'
+# content_path = tf.keras.utils.get_file('YellowLabradorLooking_new.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/YellowLabradorLooking_new.jpg')
+# style1_path = tf.keras.utils.get_file('kandinsky5.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
+content_path = 'cat.png'
+style1_path = 'color.jpg'
+style2_path = 'messy.jpg'
+style3_path = 'Mondriaan.jpg'
      
 ##Visualize the input
 
@@ -75,6 +78,7 @@ def imshow(image, title=None):
 content_image = load_img(content_path)
 style1_image = load_img(style1_path)
 style2_image = load_img(style2_path)
+style3_image = load_img(style3_path)
 
 # plt.subplot(1, 2, 1)
 # imshow(content_image, 'Content Image')
@@ -258,14 +262,42 @@ results = extractor(tf.constant(content_image))
 #   print("    mean: ", output.numpy().mean())
 
 counter = 0
-relative_weight = 0.9
+relative_weight = [0.5,0.5]
 
+relative_weight1 = relative_weight[0]
+relative_weight2 = relative_weight[0]
+
+def style_content_loss_more_color(outputs):
+      style_outputs = outputs['style']
+      content_outputs = outputs['content']
+
+      # where are those layer weights?
+
+      style1_loss = tf.add_n([tf.reduce_mean((style_outputs[name]-style1_targets[name])**2) 
+                            for name in style_outputs.keys()])
+      style1_loss *= style_weight / num_style_layers
+
+      style2_loss = tf.add_n([tf.reduce_mean((style_outputs[name]-style2_targets[name])**2) 
+                            for name in style_outputs.keys()])
+      style2_loss *= style_weight / num_style_layers
+
+      style3_loss = tf.add_n([tf.reduce_mean((style_outputs[name]-style3_targets[name])**2) 
+                            for name in style_outputs.keys()])
+      style3_loss *= style_weight / num_style_layers
+
+      content_loss = tf.add_n([tf.reduce_mean((content_outputs[name]-content_targets[name])**2) 
+                              for name in content_outputs.keys()])
+      content_loss *= content_weight / num_content_layers
+      loss = (relative_weight1 * style1_loss + relative_weight2 * style2_loss + 
+              (1-relative_weight1 - relative_weight2) * style3_loss) + content_loss 
+      return loss
 
 while counter <= 4:
 
   # Set your style and content target values:
   style1_targets = extractor(style1_image)['style']
   style2_targets = extractor(style2_image)['style']
+  style3_targets = extractor(style3_image)['style']
   content_targets = extractor(content_image)['content']
 
   # Define a tf.Variable to contain the image to optimize. To make this quick, initialize it with the content image (the tf.Variable must be the same shape as the content image):
@@ -285,6 +317,9 @@ while counter <= 4:
   def style_content_loss(outputs):
       style_outputs = outputs['style']
       content_outputs = outputs['content']
+
+      # where are those layer weights?
+
       style1_loss = tf.add_n([tf.reduce_mean((style_outputs[name]-style1_targets[name])**2) 
                             for name in style_outputs.keys()])
       style1_loss *= style_weight / num_style_layers
@@ -293,10 +328,15 @@ while counter <= 4:
                             for name in style_outputs.keys()])
       style2_loss *= style_weight / num_style_layers
 
+      style3_loss = tf.add_n([tf.reduce_mean((style_outputs[name]-style3_targets[name])**2) 
+                            for name in style_outputs.keys()])
+      style3_loss *= style_weight / num_style_layers
+
       content_loss = tf.add_n([tf.reduce_mean((content_outputs[name]-content_targets[name])**2) 
                               for name in content_outputs.keys()])
       content_loss *= content_weight / num_content_layers
-      loss = (relative_weight * style1_loss + (1 - relative_weight) * style2_loss) + content_loss 
+      loss = (relative_weight1 * style1_loss + relative_weight2 * style2_loss + 
+              (1-relative_weight1 - relative_weight2) * style3_loss) + content_loss 
       return loss
 
   # Use tf.GradientTape to update the image.
@@ -349,7 +389,24 @@ while counter <= 4:
   else:
     files.download(file_name)
   
-  relative_weight = relative_weight - 0.2
+  image1 = load_img(file_name)
+  image1 = np.squeeze(image1)
+  
+  image1_size = image1.shape
+  image1_height = image1_size[0]
+  image1_width = image1_size[1]
+
+  sum = 0
+  
+  for r in range(0,image1_height):
+    for s in range(0,image1_width):
+      for t in range(3):
+        sum += image1[r][s][t]
+
+  print(sum)
+  
+  relative_weight1 = relative_weight1 - 0.1
+  relative_weight2 = relative_weight2 - 0.1
   counter = counter + 1  
 
 import cv2
@@ -363,6 +420,13 @@ transfer_yuv[:,:,1:3] = content_yuv[:,:,1:3]
 color_preserved_transfer = cv2.cvtColor(transfer_yuv, cv2.COLOR_YUV2RGB)
 file_name = 'color-preserved-transfer.png'
 tensor_to_image(color_preserved_transfer).save(file_name)    
+try:
+  from google.colab import files
+except ImportError:
+  pass
+else:
+  files.download(file_name)
+
 
 # Sharp edges detector and eliminator
 
@@ -383,7 +447,7 @@ while counter2 <= 4:
   grad_mag = np.sqrt(dx ** 2 + dy ** 2)
   grad_ori = np.arctan2(dy, dx)
 
-  print(grad_mag.shape)
+  print(grad_mag)
 
   mixed_image_size = mixed_image.shape
   mixed_image_height = mixed_image_size[0]
@@ -396,14 +460,14 @@ while counter2 <= 4:
 
   for i in range(2,mixed_image_height-1):
     for j in range(2,mixed_image_width-1):
-      if grad_mag[i][j] > 0.3:
+      if grad_mag[i][j] > 0.40:
         for m in range(3):
           a = np.array([
               [mixed_image[i-1][j-1][m],mixed_image[i-1][j][m],mixed_image[i-1][j+1][m]],
               [mixed_image[i][j-1][m],mixed_image[i][j][m],mixed_image[i][j+1][m]],
               [mixed_image[i+1][j-1][m],mixed_image[i+1][j][m],mixed_image[i+1][j+1][m]]
               ])
-          x = filters.gaussian(a, sigma=0.4)
+          x = filters.gaussian(a, sigma=0.5)
           if m == 0:
             mixed_image[i-1][j-1] = [x[0][0],mixed_image[i-1][j-1][1],mixed_image[i-1][j-1][2]]
             mixed_image[i-1][j] = [x[0][1],mixed_image[i-1][j][1],mixed_image[i-1][j][2]]
@@ -447,95 +511,135 @@ while counter2 <= 4:
 
   counter2 += 1
 
+
+
 # Region Brightness Opmitimation
 
-mixed_image_path = 'stylized-image4.jpg'
+dict = {'stylized-image0.jpg':[0.5,0.5],'stylized-image1.jpg':[0.4,0.4],
+        'stylized-image2.jpg':[0.3,0.3],'stylized-image3.jpg':[0.2,0.2],
+        'stylized-image4.jpg':[0.1,0.1],'stylized-image_brightness0.jpg':[0.5,0.5],
+        'stylized-image_brightness1.jpg':[0.4,0.4],'stylized-image_brightness2.jpg':[0.3,0.3],
+        'stylized-image_brightness3.jpg':[0.2,0.2],'stylized-image_brightness4.jpg':[0.1,0.1]}
 
-mixed_image = load_img(mixed_image_path)
+for k in range(5):
 
-mixed_image = np.squeeze(mixed_image)
-mixed_image2 = rgb2gray(mixed_image)
+  chosen_image_path = 'stylized-image' + str(k) + '.jpg'
+  relative_weight1 = dict[chosen_image_path][0]
+  relative_weight2 = dict[chosen_image_path][1]
+  print(relative_weight1)
 
-mixed_image_size = mixed_image.shape
-mixed_image_height = mixed_image_size[0]
-mixed_image_width = mixed_image_size[1]
+  chosen_image = load_img(chosen_image_path)
 
-region_height = mixed_image_height//10
-region_width = mixed_image_width//10
+  chosen_image2 = np.squeeze(chosen_image)
 
-sum = 0
+  chosen_image_size = chosen_image2.shape
+  chosen_image_height = chosen_image_size[0]
+  chosen_image_width = chosen_image_size[1]
 
-for i in range(0,region_height):
-  for j in range(0,region_width):
-    for n in range(3):
-      sum += mixed_image[i][j][n]
+  region_height = chosen_image_height//5
+  region_width = chosen_image_width//5
 
-if sum >= 3*region_height*region_width*0.5:
-  relative_weight = 0.9-0.3
-  # Set your style and content target values:
-  style1_targets = extractor(style1_image)['style']
-  style2_targets = extractor(style2_image)['style']
-  content_targets = extractor(content_image)['content']
+  chosen_image2 = chosen_image2.copy()
 
-  # Define a tf.Variable to contain the image to optimize. To make this quick, initialize it with the content image (the tf.Variable must be the same shape as the content image):
-  image = tf.Variable(content_image)
+  for x in range(5):
+    for y in range(5):
 
-  #Create an optimizer. The paper recommends LBFGS, but Adam works okay, too:
-  opt = tf.keras.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
+      relative_weight1 = dict[chosen_image_path][0]
+      relative_weight2 = dict[chosen_image_path][1]
 
-  #To optimize this, use a weighted combination of the two losses to get the total loss:
-  style_weight=1e-2
-  content_weight=1e4
+      sum = 0
 
-  # Use tf.GradientTape to update the image.
-  total_variation_weight=30
+      if sum >= 3*region_height*region_width*0.5 or sum <= 3*region_height*region_width*0.2:
 
-  # Since it's working, perform a longer optimization:
-  import time
-  start = time.time()
-  
-  epochs = 2
-  steps_per_epoch = 100
+        sum = 0
 
-  step = 0
-  for n in range(epochs):
-    for m in range(steps_per_epoch):
-      step += 1
-      train_step(image)
-      print(".", end='', flush=True)
-    #display.clear_output(wait=True)
-    #display.display(tensor_to_image(image))
-    print("Train step: {}".format(step))
+        for i in range(x*region_height,(x+1)*region_height):
+          for j in range(y*region_width,(y+1)*region_width):
+            for n in range(3):
+              sum += chosen_image2[i][j][n]
 
-  end = time.time()
-  print("Total time: {:.1f}".format(end-start))
+        print(str(x)+str(y))
+        print(sum)
+        print(3*region_height*region_width*0.5)
 
-  file_name = 'stylized-image_brightness.jpg'
-  tensor_to_image(image).save(file_name)
+        if sum >= 3*region_height*region_width*0.5 or sum <= 3*region_height*region_width*0.3:
+          if sum >= 3*region_height*region_width*0.5:
+            relative_weight1 = relative_weight1 + 0.90
+            relative_weight2 = relative_weight1 - 0.10
+          elif sum <= 3*region_height*region_width*0.3:
+            relative_weight1 = relative_weight1 - 0.20
+            relative_weight2 = relative_weight1 - 0.20
+          # Set your style and content target values:
+          style1_targets = extractor(style1_image)['style']
+          style2_targets = extractor(style2_image)['style']
+          style3_targets = extractor(style3_image)['style']
+          content_targets = extractor(content_image)['content']
 
-  try:
-    from google.colab import files
-  except ImportError:
-    pass
-  else:
-    files.download(file_name)
+          # Define a tf.Variable to contain the image to optimize. To make this quick, initialize it with the content image (the tf.Variable must be the same shape as the content image):
+          image = tf.Variable(content_image)
 
-  new_image_path = 'stylized-image_brightness.jpg'
+          #Create an optimizer. The paper recommends LBFGS, but Adam works okay, too:
+          opt = tf.keras.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
 
-  new_image = load_img(mixed_image_path)
+          #To optimize this, use a weighted combination of the two losses to get the total loss:
+          style_weight=1e-2
+          content_weight=1e4
 
-  new_image = np.squeeze(mixed_image)
-  new_image2 = rgb2gray(mixed_image)
+          # Use tf.GradientTape to update the image.
+          total_variation_weight=30
 
-  mixed_image = mixed_image.copy()
+          # Since it's working, perform a longer optimization:
+          import time
+          start = time.time()
+          
+          epochs = 1
+          steps_per_epoch = 30
 
-  for o in range(0,region_height):
-    for p in range(0,region_width):
-      for q in range(3):
-        mixed_image[o][p][q] = new_image[o][p][q]
+          step = 0
+          for n in range(epochs):
+            for m in range(steps_per_epoch):
+              step += 1
+              train_step(image)
+              print(".", end='', flush=True)
+            #display.clear_output(wait=True)
+            #display.display(tensor_to_image(image))
+            print("Train step: {}".format(step))
 
-  file_name = 'stylized-image_brightness_new.jpg'
-  tensor_to_image(mixed_image).save(file_name)
+          end = time.time()
+          print("Total time: {:.1f}".format(end-start))
+
+          if x==0 and y == 0:
+            file_name = 'stylized-image_brightness' + str(k) + '.jpg'
+            tensor_to_image(image).save(file_name)
+
+            new_image_path = 'stylized-image_brightness' + str(k) + '.jpg'
+          
+          else:
+            file_name = 'stylized-image_brightness_iterate' + str(k) + '.jpg'
+            tensor_to_image(image).save(file_name)
+
+            new_image_path = 'stylized-image_brightness_iterate' + str(k) + '.jpg'
+
+          new_image = load_img(new_image_path)
+
+          new_image = np.squeeze(new_image)
+          new_image2 = rgb2gray(new_image)
+          new_image = new_image.copy()
+
+          for o in range(x*region_height,(x+1)*region_height):
+            for p in range(y*region_width,(y+1)*region_width):
+              for q in range(3):
+                chosen_image2[o][p][q] = new_image[o][p][q]
+
+          file_name = 'stylized-image_brightness' + str(k) + '.jpg'
+          tensor_to_image(chosen_image2).save(file_name)
+
+          chosen_image_path = 'stylized-image_brightness' + str(k) + '.jpg'
+
+          chosen_image = load_img(chosen_image_path)
+
+          chosen_image2 = np.squeeze(chosen_image)
+          chosen_image2 = chosen_image2.copy()
 
   try:
     from google.colab import files
